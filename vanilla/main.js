3819c99d87dad11c5b60a10a4ecba4ee12e6e4dd
Al Armato <55386215+zyzalfors@users.noreply.github.com>
2025-04-06 22:26:24 +0200

    Delete vanilla directory

--- vanilla/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import Config from './components/Conf.vue';
-import Messages from './components/Mess.vue';
-
-const app = createApp(App);
-app.component('Conf', Config);
-app.component('Mess', Messages);
-app.mount('#app');

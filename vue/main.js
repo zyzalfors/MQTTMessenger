0f765ebb16ef38b4cd842e01df8ee31b4c5e738d
Al Armato <55386215+zyzalfors@users.noreply.github.com>
2025-04-06 22:12:23 +0200

    Add files via upload

--- vue/main.js
+++ vue/main.js
@@ -1,9 +1,9 @@
-import {createApp} from "vue";
-import App from "./App.vue";
-import Conf from "./components/Conf.vue";
-import Mess from "./components/Mess.vue";
+import { createApp } from 'vue';
+import App from './App.vue';
+import Config from './components/Conf.vue';
+import Messages from './components/Mess.vue';
 
 const app = createApp(App);
-app.component("Conf", Conf);
-app.component("Mess", Mess);
-app.mount('#app')
+app.component('Conf', Config);
+app.component('Mess', Messages);
+app.mount('#app');
